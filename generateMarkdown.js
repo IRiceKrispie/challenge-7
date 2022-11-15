7c5078e643f8a332c3ad4456a5e5d48762ac9d9c
IRiceKrispie <joseph.sandoval.cs6@gmail.com>
2022-11-15 11:08:47 -0600

    cleaned up readme code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -30,30 +30,25 @@ function renderLicenseLink(license) {
 
 //function to generate our markdown file
 function generateMarkdown(data) {
-    return `# ${data.projectTitle}
-    ## Table of Contents
-    [Description](#description)
-    [Installation Instructions](#installation)
-    [Usage](#usage)
-    [Questions](#questions)
-    [License](#license)
-    <a name="description"/>
-    ## Description
-    * ${data.description}
-    <a name="install"/>
-    ## Installation
-    * ${data.installation}
-    <a name="usage"/>
-    ## Usage
-    * ${data.usage}
-    <a name="questions"/>
-    ## Questions
-    * Profile: https://github.com/${data.userName}
-    * Email: ${data.email}
-    <a name="license"/>
-    # License
-    * ${renderLicenseBadge(data.license)}
-    * Link: ${renderLicenseLink(data.license)}`;
-  }
+  return `# ${data.projectTitle}
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation Instructions](#installation)
+  * [Usage](#usage)
+  * [Questions](#questions)
+  * [License](#license)
+  ## Description
+  * ${data.description}
+  ## Installation
+  * ${data.installation}
+  ## Usage
+  * ${data.usage}
+  ## Questions
+  * Profile: https://github.com/${data.userName}
+  * Email: ${data.email}
+  # License
+  ${renderLicenseBadge(data.license)}
+  Link: ${renderLicenseLink(data.license)}`;
+}
 
-  module.exports = generateMarkdown; //exports our functions for use in another file
+module.exports = generateMarkdown; //exports our functions for use in another file
